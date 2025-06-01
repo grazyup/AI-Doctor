05a399095340cce4f61d8efe6c7b29b13dc8a5dc
grazy <grazyup@163.com>
2025-06-01 22:46:43 +0800

    初始化Ai-Doctor项目

---
+++ src/main/java/com/grazy/AiDoctorApplication.java
@@ -0,0 +1,13 @@
+package com.grazy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDoctorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiDoctorApplication.class, args);
+    }
+
+}
